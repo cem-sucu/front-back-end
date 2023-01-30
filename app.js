2db6efc02376293fc59520629e90759de5961e19
cem-sucu <cems.cems.17@gmail.com>
2023-01-30 16:27:50 +0100

    création route POST

--- app.js
+++ app.js
@@ -2,6 +2,8 @@ const express = require("express");
 
 const app = express();
 
+app.use(express.json());
+
 app.use((req, res, next) => {
     res.setHeader("Access-Control-Allow-Origin", "*");
     res.setHeader(
@@ -15,6 +17,12 @@ app.use((req, res, next) => {
     next();
 });
 
+app.post("/api/stuff", (req, res, next) => {
+    console.log(req.body);
+    res.status(201).json({
+        message: "objet créé",
+    });
+});
 app.use("/api/stuff", (req, res, next) => {
     const stuff = [
         {
